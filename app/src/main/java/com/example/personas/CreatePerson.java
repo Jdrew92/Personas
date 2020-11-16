2d2eb1c651fa1c5003d7ada96c94ed91d8aea9d5
Jandres <darkkhir0@gmail.com>
2020-11-16 14:13:19 -0500

    Crear persona terminado, bilingue y con validacion

--- app/src/main/java/com/example/personas/CreatePerson.java
+++ app/src/main/java/com/example/personas/CreatePerson.java
@@ -3,12 +3,76 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreatePerson extends AppCompatActivity {
+
+    private EditText dni, firstName, lastName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_person);
+
+        dni = findViewById(R.id.txtDNI);
+        firstName = findViewById(R.id.txtFirstName);
+        lastName = findViewById(R.id.txtLastName);
+
+    }
+
+    public void save(View v){
+        String ced, nom, ape;
+        Persona p;
+
+        ced = dni.getText().toString();
+        nom = firstName.getText().toString();
+        ape = lastName.getText().toString();
+
+        if(validate(ced, nom, ape)) {
+
+            p = new Persona(nom, ape, ced);
+            p.save();
+            clean();
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+
+            Toast.makeText(this, R.string.toast_person, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void clean(View v){
+       clean();
+    }
+
+    public void clean(){
+        dni.setText("");
+        firstName.setText("");
+        lastName.setText("");
+        dni.requestFocus();
+    }
+
+    public boolean validate(String ced, String nom, String ape){
+        if(ced.isEmpty()){
+            dni.setError(getText(R.string.empty_dni));
+            dni.requestFocus();
+            return false;
+        }
+        if(nom.isEmpty()){
+            firstName.setError(getText(R.string.empty_fname));
+            firstName.requestFocus();
+            return false;
+        }
+        if (ape.isEmpty()){
+            lastName.setError(getText(R.string.empty_lname));
+            lastName.requestFocus();
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
